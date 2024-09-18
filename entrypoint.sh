eb6dcf94d1c904cad9a8a2ed02e49e8c2daaea14
Tanner Lewis <lewijacn@amazon.com>
2024-09-18 15:42:05 -0500

    Update env arg parsing
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,12 +3,16 @@
 printenv
 
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
-  branch_name="${PR_BRANCH}"
-  pr_repo_url="${PR_REPO}"
+  branch_name="${{ github.event.pull_request.head.ref }}"
+  #pr_repo_url="https://github.com/${{ github.event.pull_request.head.repo.full_name }}.git"
 else
-  branch_name="${PUSH_BRANCH}"
-  pr_repo_url="${PUSH_REPO}"
+  branch_name="${GITHUB_REF_NAME}"
+  #pr_repo_url="https://github.com/${{ github.repository }}.git"
 fi
+pr_repo_url="https://github.com/${GITHUB_REPOSITORY}.git"
+
+
+
 echo "Running jenkins test on repo: $pr_repo_url and branch: $branch_name"
 pipenv run python3 default_webhook_trigger.py \
   --jenkins_url="${INPUT_JENKINS_URL}" \
