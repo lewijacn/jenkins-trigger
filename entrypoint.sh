6220b827ea6bc5e5e1dd934415cc7dc5bc7ceb0c
Tanner Lewis <lewijacn@amazon.com>
2024-09-18 15:44:10 -0500

    Fix entrypoint bug
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,6 +17,6 @@ echo "Running jenkins test on repo: $pr_repo_url and branch: $branch_name"
 pipenv run python3 default_webhook_trigger.py \
   --jenkins_url="${INPUT_JENKINS_URL}" \
   --pipeline_token="${INPUT_API_TOKEN}" \
-  --job_name="${INPUT_JOB_NAME }" \
+  --job_name="${INPUT_JOB_NAME}" \
   --job_params="${INPUT_JOB_PARAMS}" \
   --job_timeout_minutes="${INPUT_JOB_TIMEOUT_MINUTES}"
