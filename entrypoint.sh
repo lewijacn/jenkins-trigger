5051f56a5294c74ec8e14e40a8770a29ece8974a
Tanner Lewis <lewijacn@amazon.com>
2024-09-18 15:36:26 -0500

    Update arg passing
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,16 @@
 printenv
 
 if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
-  branch_name="${{ github.event.pull_request.head.ref }}"
-  pr_repo_url="https://github.com/${{ github.event.pull_request.head.repo.full_name }}.git"
+  branch_name="${PR_BRANCH}"
+  pr_repo_url="${PR_REPO}"
 else
-  branch_name="${{ github.ref_name }}"
-  pr_repo_url="https://github.com/${{ github.repository }}.git"
+  branch_name="${PUSH_BRANCH}"
+  pr_repo_url="${PUSH_REPO}"
 fi
 echo "Running jenkins test on repo: $pr_repo_url and branch: $branch_name"
 pipenv run python3 default_webhook_trigger.py \
-  --jenkins_url="${{ INPUT_JENKINS_URL }}" \
-  --pipeline_token="${{ INPUT_API_TOKEN }}" \
-  --job_name="${{ INPUT_JOB_NAME }}" \
-  --job_params="${{ INPUT_JOB_PARAMS }}" \
-  --job_timeout_minutes="${{ INPUT_JOB_TIMEOUT_MINUTES }}"
+  --jenkins_url="${INPUT_JENKINS_URL}" \
+  --pipeline_token="${INPUT_API_TOKEN}" \
+  --job_name="${INPUT_JOB_NAME }" \
+  --job_params="${INPUT_JOB_PARAMS}" \
+  --job_timeout_minutes="${INPUT_JOB_TIMEOUT_MINUTES}"
