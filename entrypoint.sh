938bb742b68f5b5c4f2d05c678d255d71b6f51ec
Tanner Lewis <lewijacn@amazon.com>
2024-09-19 09:35:00 -0500

    Remove git logic from action
    Signed-off-by: Tanner Lewis <lewijacn@amazon.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
 
-printenv
-
-if [[ "${GITHUB_EVENT_NAME}" == "pull_request" ]]; then
-  branch_name="${{ github.event.pull_request.head.ref }}"
-  #pr_repo_url="https://github.com/${{ github.event.pull_request.head.repo.full_name }}.git"
-else
-  branch_name="${GITHUB_REF_NAME}"
-  #pr_repo_url="https://github.com/${{ github.repository }}.git"
-fi
-pr_repo_url="https://github.com/${GITHUB_REPOSITORY}.git"
-
-
 cd /webhook-trigger || exit
-echo "Running jenkins test on repo: $pr_repo_url and branch: $branch_name"
+echo "Executing jenkins webhook trigger for url: $INPUT_JENKINS_URL and job: $INPUT_JOB_NAME"
 python3 default_webhook_trigger.py \
   --jenkins_url="${INPUT_JENKINS_URL}" \
   --pipeline_token="${INPUT_API_TOKEN}" \
